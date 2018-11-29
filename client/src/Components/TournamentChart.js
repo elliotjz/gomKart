e0176ff5ff99c10c45999b65015afbf7987e36e8
Elliot Zoerner <eliot.zoerner@gmail.com>
2018-11-29 01:46:44 +0100

    can change the domain for the chart

--- client/src/Components/TournamentChart.js
+++ client/src/Components/TournamentChart.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withStyles } from '@material-ui/core/styles'
 import { Chart } from 'react-google-charts'
 import Typography from '@material-ui/core/Typography'
+import { Button } from '@material-ui/core'
 
 import { colors } from '../helpers'
 import PlayerChips from './PlayerChips'
@@ -19,28 +20,67 @@ const chartOptions = {
   chartArea: {width: '85%', height: '80%'}
 }
 
-const TournamentChart = ({ players, parsedData, classes }) => (
-  <div>
-    {(parsedData !== undefined && parsedData[0].length > 1) ?
-      <div className={classes.root}>
-          <PlayerChips
-          players={players}
-          parsedData={parsedData}
-          colors={colors}
-          />
-        <Chart
-          chartType="LineChart"
-          width="100%"
-          height="600px"
-          data={parsedData}
-          options={chartOptions}
-        />
-      </div> :
-      <div>
-        <Typography variant="p">You need to add players to the tournament before you can see the chart</Typography>
-      </div>
+const chartDomains = ['All', 400, 100, 50]
+
+class TournamentChart extends Component {
+  constructor(props) {
+    super(props)
+    this.changeDomain = this.changeDomain.bind(this)
+    this.state = {
+      chartDomainIndex: 100
     }
-  </div>
-)
+  }
+
+  changeDomain(index) {
+    this.setState({
+      chartDomainIndex: index
+    })
+
+  }
+
+  render() {
+    const { players, parsedData, classes } = this.props
+    const { chartDomainIndex } = this.state
+    let dataToGraph = []
+    const chartDomain = chartDomains[chartDomainIndex]
+    if (chartDomainIndex !== 0 && chartDomain < parsedData.length) {
+      const endIndex =  parsedData.length - 1
+      const startIndex = endIndex - chartDomain
+      dataToGraph.push(parsedData[0])
+      dataToGraph.push.apply(dataToGraph, parsedData.slice(startIndex, endIndex))
+    } else {
+      dataToGraph = parsedData
+    }
+
+    return (
+      <div>
+        {(dataToGraph !== undefined && dataToGraph[0].length > 1) ?
+          <div className={classes.root}>
+              <PlayerChips
+              players={players}
+              parsedData={dataToGraph}
+              colors={colors}
+              />
+            <Chart
+              chartType="LineChart"
+              width="100%"
+              height="600px"
+              data={dataToGraph}
+              options={chartOptions}
+            />
+            {chartDomains.map((domain, index) =>
+              <Button key={index} color="primary" onClick={() => this.changeDomain(index)}>
+                {domain}
+              </Button>
+              )}
+          </div> :
+          <div>
+            <Typography variant="p">You need to add players to the tournament before you can see the chart</Typography>
+          </div>
+        }
+      </div>
+    )
+  }
+}
 
 export default withStyles(styles)(TournamentChart)
